3b49d1cf3de7f8dcd326c1a31c89500a96426a1c
geraldinenunez-leguizamon-tm <geraldine.nunez.leguizamon@toyota.com>
2022-04-13 11:34:32 -0500

    Code base

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders todos app text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/todo's app/i);
   expect(linkElement).toBeInTheDocument();
 });
